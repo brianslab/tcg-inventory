801dde163000b0f9531a16d66c74f8b6e055be98
Brian Kuhn <bdkuhn602@gmail.com>
2021-10-22 11:20:38 -0700

    add simple cli parsing

--- tcg_inv.py
+++ tcg_inv.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
+import sys, getopt
 
+# Initialize client
 scope = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
@@ -8,8 +12,30 @@ scope = [
 creds = ServiceAccountCredentials.from_json_keyfile_name('tcg-inventory-SA.json', scope)
 client = gspread.authorize(creds)
 
-storage = client.open("MTG Cards").worksheet("storage")
-breena = client.open("MTG Cards").worksheet("EDH-Breena")
+# Iinitialiaze sheets
+inv = "MTG Cards"
 
-storage.update_cell(1,3, "Hello World!")
-breena.update_cell(1,3, "Hello World!")
+storage = client.open(inv).worksheet("storage")
+breena = client.open(inv).worksheet("EDH-Breena")
+nicol = client.open(inv).worksheet("EDH-Nicol Bolas")
+child = client.open(inv).worksheet("EDH-Child of Alara")
+etron = client.open(inv).worksheet("MOD-Eldrazi Tron")
+
+# Parse arguments
+addfile = ""
+to = storage.title
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "ha:t:", ["add=", "to="])
+except:
+    print("ERROR: invalid arguments. See -h")
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == "-h":
+        print("-add : adds cards from file")
+        sys.exit()
+    elif opt in ("-a", "--add"):
+        addfile = arg
+    elif opt in ("-t", "--to"):
+        to = arg
+
+print("adding cards from", addfile, "to", to)
